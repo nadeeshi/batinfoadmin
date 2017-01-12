ad4c14631c725473e5c3b0df6dc6942a28ecbf85
Nadeeshika senavirathna <nadeesenavirathna@gmail.com>
2017-01-12 21:25:52 +0530

    online db

--- assets/includedFiles/connect.php
+++ assets/includedFiles/connect.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "project";
+$mysql_hostname = "us-cdbr-azure-southcentral-f.cloudapp.net";
+$mysql_user = "ba24a8e6ef7756";
+$mysql_password = "b8bded59";
+$mysql_database = "bat";
 $prefix = "";
 $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysqli_select_db($bd ,$mysql_database) or die("Could not select database");
